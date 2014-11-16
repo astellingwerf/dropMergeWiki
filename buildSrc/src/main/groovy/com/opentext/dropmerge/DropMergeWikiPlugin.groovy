efc6c73fd4654422538870493026199901fa041a
Anne Stellingwerf <astellingwerf@cordys.com>
2014-11-16 23:15:38 +0100

    Simplifed NDOCs.

--- buildSrc/src/main/groovy/com/opentext/dropmerge/DropMergeWikiPlugin.groovy
+++ buildSrc/src/main/groovy/com/opentext/dropmerge/DropMergeWikiPlugin.groovy
@@ -15,9 +15,9 @@ class DropMergeWikiPlugin implements Plugin<Project>{
     }
 
     void applyConventions(project) {
-        def servers = project.container(JenkinsServer) { name -> new JenkinsServer(name) }
-        def jobs = project.container(JenkinsJob) { name -> new JenkinsJob(name) }
-        def regressionTests = project.container(RegressionTest) { name -> new RegressionTest(name) }
+        def servers = project.container(JenkinsServer)
+        def jobs = project.container(JenkinsJob)
+        def regressionTests = project.container(RegressionTest)
 
         def configuration = new DropMergeConfiguration(servers, jobs, regressionTests)
         project.convention.plugins.dropMerge = new DropMergeConfigurationConvention(configuration)
