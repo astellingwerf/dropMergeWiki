3d7e9f2e4d9b2e950e44971b513fe1ec0c13455c
Anne Stellingwerf <astellingwerf@cordys.com>
2014-11-17 00:34:05 +0100

    Added DSL for regression tests.

--- buildSrc/src/main/groovy/com/opentext/dropmerge/dsl/RegressionTest.groovy
+++ buildSrc/src/main/groovy/com/opentext/dropmerge/dsl/RegressionTest.groovy
@@ -1,9 +1,34 @@
 package com.opentext.dropmerge.dsl
 
+import org.apache.commons.lang3.tuple.Pair
+
 class RegressionTest {
     String name
 
+    Collection<Pair<Collection<JenkinsJob>, Collection<JenkinsJob>>> comparables = []
+    Collection<JenkinsJob> others = []
+
     RegressionTest(String name) {
         this.name = name
+    }
+
+    void compare(JenkinsJob wip, JenkinsJob trunk) {
+        compare([wip], [trunk])
+    }
+
+    void compare(Collection<JenkinsJob> wip, Collection<JenkinsJob> trunk) {
+        comparables += Pair.of(wip, trunk)
+    }
+
+    void others(JenkinsJob... others) {
+        this.others.addAll(others)
+    }
+
+    @Override
+    public String toString() {
+        return "$name => comparables=" + comparables.collect {
+            Pair.of(it.left.collect { it.name }, it.right.collect { it.name })
+        } +
+                ", others=" + others.collect { it.name }
     }
 }
