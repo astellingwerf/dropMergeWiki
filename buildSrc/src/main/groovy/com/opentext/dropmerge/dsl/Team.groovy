1bdb09b5ec44ecb53a29aba067a814aca826b05a
Anne Stellingwerf <astellin@opentext.com>
2015-01-03 16:12:54 +0100

    Cleanup Team.groovy

--- buildSrc/src/main/groovy/com/opentext/dropmerge/dsl/Team.groovy
+++ buildSrc/src/main/groovy/com/opentext/dropmerge/dsl/Team.groovy
@@ -3,32 +3,34 @@ package com.opentext.dropmerge.dsl
 
 class Team {
     String name
-    String scrumMaster
+    List<String> scrumMaster = []
     List<String> architects = []
-    String productManager
+    List<String> productManager = []
     List<String> otherMembers = []
 
     void name(String name) { this.name = name }
 
-    void scrumMaster(String scrumMaster) { this.scrumMaster = scrumMaster }
+    void scrumMaster(String... scrumMaster) { this.scrumMaster.addAll(scrumMaster) }
 
     void architect(String... architect) { this.architects.addAll(architect) }
 
-    void productManager(String productManager) { this.productManager = productManager }
+    void productManager(String... productManager) { this.productManager.addAll(productManager) }
 
     void otherMembers(String... member) { this.otherMembers.addAll(member) }
 
     List<String> getAllMembers() {
-        ([scrumMaster, productManager] + architects + otherMembers)
+        (scrumMaster + productManager + architects + otherMembers).sort().unique()
     }
 
     @Override
     public String toString() {
         return name + ': ' +
-                allMembers.sort().unique().collect {
-                    def roles = (scrumMaster == it ? ['S'] : []) +
-                            (productManager == it ? ['P'] : []) +
-                            (architects.contains(it) ? ['A'] : [])
+                allMembers.collect {
+                    def roles = [A: this.&getArchitects,
+                                 P: this.&getProductManager,
+                                 S: this.&getScrumMaster].collectMany { role ->
+                        (role.value().contains(it) ? [role.key] : [])
+                    }
                     it + (roles ? " (${roles.join(',')})" : '')
                 }.join(', ')
     }
